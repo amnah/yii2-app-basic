8efb2730909907e4d429ece323291ceb2ca9630b
amnah <amnah.dev@gmail.com>
2018-03-18 12:59:23 -0400

    update console config

--- config/console.php
+++ config/console.php
@@ -1,48 +1,24 @@
 <?php
 
-$params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/db.php';
+$config = require_once __DIR__ . '/web.php';
 
-$config = [
-    'id' => 'basic-console',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'app\commands',
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-        '@tests' => '@app/tests',
+$config['id'] .= '-console';
+$config['controllerNamespace'] = 'app\commands';
+$config['aliases']['@tests'] = '@app/tests';
+
+unset($config['components']['request']);
+unset($config['components']['user']);
+unset($config['components']['session']);
+unset($config['components']['errorHandler']);
+unset($config['components']['log']['traceLevel']);
+unset($config['components']['urlManager']);
+
+/*
+$config['controllerMap'] = [
+    'fixture' => [ // Fixture generation command line.
+        'class' => 'yii\faker\FixtureController',
     ],
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => $db,
-    ],
-    'params' => $params,
-    /*
-    'controllerMap' => [
-        'fixture' => [ // Fixture generation command line.
-            'class' => 'yii\faker\FixtureController',
-        ],
-    ],
-    */
 ];
-
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
+*/
 
 return $config;
